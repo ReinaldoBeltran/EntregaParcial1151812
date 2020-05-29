668239f7f2a14c3770356f53234b85d9035da5d9
Reinaldo <reinaldosb@ufps.edu.co>
2020-05-29 11:48:04 -0500

    comic1
    PRIMER  COMIT

---
+++ src/Dao/Conexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Conexion {
+    
+    private static Conexion conexion;
+    private EntityManagerFactory bd; 
+
+    private  Conexion() {
+        //se coloca la conexion.. de la persistencia.
+        this.bd=Persistence.createEntityManagerFactory("com.maven_SegundoPrevio_war_0.0.1-SNAPSHOTPU");
+        
+    }
+    
+    
+    public static Conexion getConexion()
+    {
+        if(conexion==null)
+        {
+            conexion=new Conexion();
+        }
+    return conexion;
+    }
+
+    public EntityManagerFactory getBd() {
+        return bd;
+    }
+    
+    
+    
+    
+}
---
+++ src/Test/java/Prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test.java;
+
+/**
+ *
+ * @author XCand
+ */
+public class Prueba {
+    
+    
+    static void Main(string[] args)
+    {
+        //insert code
+        
+    }
+    
+}
